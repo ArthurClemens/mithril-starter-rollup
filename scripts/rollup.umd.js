71e392b8ac1458734fbfed3c0978dbfcb8939669
Arthur Clemens <arthurclemens@gmail.com>
2016-12-23 12:51:56 +0100

    Read environment variable

--- scripts/rollup.umd.js
+++ scripts/rollup.umd.js
@@ -4,7 +4,8 @@ Builds a UMD bundle
 import { pkg, createConfig } from "./rollup.base.js";
 import uglify from "rollup-plugin-uglify";
 
-const includeDepencies = true; // Use `false` if you are creating a library, or if you are including external script in html
+const env = process.env; // eslint-disable-line no-undef
+const includeDepencies = !!env.DEPS || false; // Use `false` if you are creating a library, or if you are including external script in html
 
 const baseConfig = createConfig({ includeDepencies });
 const umdConfig = Object.assign({}, baseConfig, {
